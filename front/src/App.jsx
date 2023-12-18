f5fd8ed6d16df9e80f994687cc73e7ba4af6a011
Matis Dene <matis.dene@edu.devinci.fr>
2023-12-18 16:43:28 +0100

    :bug: update import aws

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,7 +7,7 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from './aws-exports';
+import awsExports from '../src/aws-exports';
 Amplify.configure(awsExports);
 
 export default function App() {
