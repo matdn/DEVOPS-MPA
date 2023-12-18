ecc46f9cc80249b8ffe84b3c1bca367766abff24
Matis Dene <matis.dene@edu.devinci.fr>
2023-12-18 16:42:23 +0100

    :bug: update dev

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,63 +2,7 @@ import React from 'react';
 import { Amplify } from 'aws-amplify';
 // import { Button, Flex, ThemeProvider, Theme } from '@aws-amplify/ui-react';
 
-const theme = {
-  name: 'button-theme',
-  tokens: {
-    colors: {
-      border: {
-        // this will affect the default button's border color
-        primary: { value: 'black' },
-      },
-    },
-    components: {
-      button: {
-        // this will affect the font weight of all button variants
-        fontWeight: { value: '{fontWeights.extrabold}' },
-        backgroundColor: { value: '#f1fff5' },
-        borderColor: { value: '{colors.purple.80}' },
-        color: { value: '{colors.purple.100}' },
-        outlined: {
-          info: {
-            borderColor: '{colors.purple.60}',
-            color: '{colors.purple.90}',
-          },
-        },
 
-        // style the primary variation
-        primary: {
-          backgroundColor: { value: '{colors.blue.60}' },
-          _hover: {
-            backgroundColor: { value: '{colors.blue.80}' },
-          },
-          _focus: {
-            backgroundColor: { value: '{colors.blue.80}' },
-          },
-          _active: {
-            backgroundColor: { value: '{colors.blue.90}' },
-          },
-          _disabled: {
-            backgroundColor: { value: 'transparent' },
-            borderColor: { value: '{colors.neutral.30}' },
-          },
-          error: {
-            backgroundColor: { value: '{colors.pink.10}' },
-            color: { value: '{colors.red.80}' },
-            _hover: {
-              backgroundColor: { value: '#a51b34' },
-            },
-            _focus: {
-              backgroundColor: { value: '#9a0c26' },
-            },
-            _active: {
-              backgroundColor: { value: '#9a0c26' },
-            },
-          },
-        },
-      },
-    },
-  },
-};
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
