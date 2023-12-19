4fd4187531c2ca4c5251d04f6b1ab1f427a3932b
Arthur <arthurlucas92@orange.fr>
2023-12-19 09:18:38 +0100

    :bug: try to fix import awsExports bug

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
-import { Amplify } from 'aws-amplify';
+import React from "react";
+import { Amplify } from "aws-amplify";
 // import { Button, Flex, ThemeProvider, Theme } from '@aws-amplify/ui-react';
 
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from '../src/aws-exports';
-Amplify.configure(awsExports);
+// import awsExports from '../src/aws-exports';
+// import awsExports from "./aws-exports";
+Amplify.configure({
+  aws_project_region: "eu-west-1",
+  aws_dynamodb_all_tables_region: "eu-west-1",
+  aws_dynamodb_table_schemas: [
+    {
+      tableName: "User-devarthur",
+      region: "eu-west-1",
+    },
+  ],
+});
 
 export default function App() {
-  
   return (
     <Authenticator>
-      
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
